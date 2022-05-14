efefc4a8aca505c50d9d7c20dbf3cddbe97a5871
lorenzo <lorenzo@lorenzo-VirtualBox>
2022-05-14 14:13:25 +0200

    Práctica PHP PhpDocumentor

---
+++ Function/practica-toledopelaez.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Laurent Toledo
+ * @version 3.3.1
+ * @access private
+ */
+
+ /**
+ * @var integer 
+ * @param mixed $n1
+ * @param mixed $n2
+ * @return $suma
+ */
+
+function sumar($n1, $n2) {
+    /**
+    * Comentarios 
+    */
+    $suma = $n1+$n2;
+    return $suma;
+}
+
+/**
+ * @param mixed &$suma
+ * @var integer 
+ * @return integer
+ */
+function dividir(&$suma) {
+    /**
+    * @ignore
+    */
+    $dividir = $suma/2;
+    return $dividir;
+}
+
+/**
+ * @global int 
+ */
+$resultado = sumar(30,20);
+echo dividir($resultado);
+
+?>
+
---
+++ docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\dividir\u0028\u0029",
+            "name": "dividir",
+            "summary": "",
+            "url": "namespaces/default.html#function_dividir"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
